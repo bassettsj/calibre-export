0595082f1313d7aaa6f59a0a5e4c52c406027c88
Steven Bassett <steven.j.bassett@gmail.com>
2018-07-19 16:06:28 -0700

    reportig

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 import {
   Site,
-  Snapshot,
 } from 'calibre';
 import Page from 'calibre/src/api/page';
+import SnapshotMetrics from 'calibre/src/api/snapshot-metrics';
 
 export default async () => {
   try {
     const sites = await Site.list();
     for (const site of sites) {
-      const [snapshots, pages] = await Promise.all([
-        Snapshot.list({ site: site.slug}),
-        Page.list({ site: site.slug }),
-      ]);
-      site.pages = pages;
-      site.snapshots = snapshots;
+      const pages = await Page.list({ site: site.slug });
+      for (const page of pages) {
+        const metrics = await SnapshotMetrics.pulse({
+          site: site.slug,
+          page: page.uuid,
+          metrics: ['first-meaningful-paint'],
+          durationInDays: 1,
+        });
+        page.metrics = metrics;
+      }
+      site.pages = pages
     }
     return sites;
   } catch (e) {
